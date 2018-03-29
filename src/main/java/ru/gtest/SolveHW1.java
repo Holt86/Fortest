851806a59add6e93afe9cc9f5e9892a9f098306e
Holt86 <aleksandr_986@inbox.ru>
2018-03-29 19:07:35 +0300

    after merge - 4

--- src/main/java/ru/gtest/SolveHW1.java
+++ src/main/java/ru/gtest/SolveHW1.java
@@ -8,6 +8,7 @@ public class SolveHW1 {
   public static void main(String[] args) {
     System.out.println("HW1 - 2");
     System.out.println("HW2 - 2");
+    System.out.println("HW3 - 2");
   }
 
 }
