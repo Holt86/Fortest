02c6afffb4d743bfe007e4202bd38cb7ec7f26a0
Holt86 <aleksandr_986@inbox.ru>
2018-03-29 18:40:36 +0300

    3 commit

--- src/main/java/ru/gtest/Main.java
+++ src/main/java/ru/gtest/Main.java
@@ -7,5 +7,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("First commit");
     System.out.println("2 commit");
+    System.out.println("3 commit");
   }
 }
